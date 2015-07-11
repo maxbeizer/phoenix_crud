307320b88d4ffba96215a7cba97fe1358de8688c
Max Beizer <max.beizer@gmail.com>
2015-07-10 23:15:13 -0500

    Initial commit

---
+++ lib/phoenix_crud/repo.ex
@@ -0,0 +1,9 @@
+defmodule PhoenixCrud.Repo do
+  use Ecto.Repo,
+    otp_app: :phoenix_crud,
+    adapter: Ecto.Adapters.Postgres
+
+  def priv do
+    app_dir(:phoenix_crud, "priv/repo")
+  end
+end
