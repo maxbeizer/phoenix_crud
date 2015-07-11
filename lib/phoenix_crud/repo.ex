2c5455dd829f3d58c815037c8ef8416337dec75f
Max Beizer <max.beizer@gmail.com>
2015-07-10 23:20:51 -0500

    Update phoenix version

--- lib/phoenix_crud/repo.ex
+++ lib/phoenix_crud/repo.ex
@@ -1,9 +1,3 @@
 defmodule PhoenixCrud.Repo do
-  use Ecto.Repo,
-    otp_app: :phoenix_crud,
-    adapter: Ecto.Adapters.Postgres
-
-  def priv do
-    app_dir(:phoenix_crud, "priv/repo")
-  end
+  use Ecto.Repo, otp_app: :phoenix_crud
 end
